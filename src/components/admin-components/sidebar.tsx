de3f21f7eda4cd88d8d5eb62f4d873fac46e579d
Abhinav Gadekar <abhinavgadekar4529@gmail.com>
2024-10-11 13:11:20 +0530

    items details design updated

--- src/components/admin-components/sidebar.tsx
+++ src/components/admin-components/sidebar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import logo from "@/assets/logo.svg";
+import logo from "@/assets/logo.png";
 import Link from "next/link";
 import {buttonVariants} from "@/components/ui/button";
 import {cn} from "@/lib/utils";
